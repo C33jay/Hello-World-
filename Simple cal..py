fa85ea54e6498541561d201df62d0fb20b633632
C33jay <60854189+C33jay@users.noreply.github.com>
2020-03-26 10:26:11 +0100

    Initial commit

---
+++ Simple cal..py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# #PROGRAM TO CALCULATE THE AREA OF A CIRCLE
+# 
+
+# In[ ]:
+
+
+Radius = input('Please enter your radius value:') 
+Pi= 3.142
+Area = Pi * int(Radius)**2
+print(Area)
+
